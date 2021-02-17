47350420b20a6a33d1e437641cd4862cdfc2591f
zhaoliangliangsx <zll01010@163.com>
2021-02-17 21:35:45 +0800

    第一次提交

---
+++ Java/idea/workspace/TestProject/src/com/zll/day0217/test01.java
@@ -0,0 +1,13 @@
+package com.zll.day0217;
+
+/**
+ * @Auther: zll
+ * @Date: 2021/2/17 - 20:32
+ * @Description: com.zll.day0217
+ * @version: 1.0
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
